61d29be7ae60803d52aab62ae49fe860edbb7aec
Dieguidev <diegogaraycullas@gmail.com>
2023-09-29 12:41:01 -0500

    estilizando las tarjetas de ordenes de compra

--- src/components/OrdersCard.jsx
+++ src/components/OrdersCard.jsx
@@ -1,15 +1,20 @@
-import { XMarkIcon } from "@heroicons/react/24/solid"
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 export const OrdersCard = props => {
   const { totalPrice, totalProducts } = props
 
   return (
-    <div className='flex justify-between items-center mb-3 border border-black'>
-      <p>
-        <span>01.02.23</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
-      </p>
+    <div className='w-80 p-4 flex justify-between items-center  border border-black rounded-lg mb-4'>
+      <div className='flex justify-between w-full'>
+        <p className='flex flex-col'>
+          <span className='font-light'>01.02.23</span>
+          <span className='font-light'>{totalProducts} articles</span>
+        </p>
+        <p className='flex items-center gap-2'>
+          <span className='font-medium text-2xl'>${totalPrice}</span>
+          <ChevronRightIcon className='w-6 h-6 txet-black'></ChevronRightIcon>
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,20 +1,20 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../context"
-import { OrderCard } from "../components/OrderCard"
 import { Link } from "react-router-dom"
+import { OrdersCard } from "../components/OrdersCard"
 
 export const MyOrders = () => {
   const context = useContext(ShoppingCartContext)
   return (
     <div>
-      <div className='flex items-center w-80 relative justify-center'>
-        <h1>My Orders</h1>
+      <div className='flex items-center w-80 relative justify-center mb-4'>
+        <h1 className='font-medium text-xl'>My Orders</h1>
       </div>
       <div>
         {
           context.order.map((order, index) => (
             <Link key={index} to={`/my-orders/${index}`}>
-              <OrderCard
+              <OrdersCard
                 totalPrice={order.totalPrice}
                 totalProducts={order.totalProducts}
               />
