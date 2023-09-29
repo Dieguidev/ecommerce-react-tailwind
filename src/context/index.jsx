f5a11876ea59ab2512fa9abb2bc5f74a9216f26a
Dieguidev <diegogaraycullas@gmail.com>
2023-09-29 13:23:26 -0500

    filtrando objetos por titulo

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,9 +1,34 @@
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+
+  //get products from api
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(res => res.json())
+      .then(data => setItems(data))
+  }, [])
+
+  //get products from api by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  //products filtered
+  const [filteredItems, setFilteredItems] = useState(null)
+  const filterItemsbyTitle = (items, searchByTitle) => {
+    return items.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+  }
+
+  useEffect(() => {
+    if (searchByTitle) setFilteredItems(filterItemsbyTitle(items, searchByTitle))
+    else setFilteredItems(items)
+  }, [items, searchByTitle])
+
+
   // Count of products in cart
   const [count, setCount] = useState(0);
 
@@ -52,7 +77,12 @@ export const ShoppingCartProvider = ({ children }) => {
       openCheckoutSideMenu,
       closeCheckoutSideMenu,
       order,
-      setOrder
+      setOrder,
+      items,
+      setItems,
+      searchByTitle,
+      setSearchByTitle,
+      filteredItems
     }}>
       {children}
     </ShoppingCartContext.Provider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,30 @@
-import { useState, useEffect } from "react"
+import { useContext } from "react"
 import { Card } from "../components/Card"
 import { ProductDetail } from "../components/ProductDetail"
+import { ShoppingCartContext } from "../context"
 
 
 
 
 export const Home = () => {
-  const [items, setItems] = useState(null);
 
-  useEffect(() => {
-    fetch('https://api.escuelajs.co/api/v1/products')
-      .then(res => res.json())
-      .then(data => setItems(data))
-  }, [])
+  const context = useContext(ShoppingCartContext)
 
-
+//searchByTitle,setSearchByTitle
   return (
     <>
+      <div className='flex items-center justify-center relative w-80 mb-4'>
+        <h1 className='font-medium text-xl'>Exclusive Products</h1>
+      </div>
+      <input
+        type="text"
+        placeholder='Search product'
+        className='rounded-lg  border border-black w-80 p-4 mb-4 focus:outline-none'
+        onChange={(e) => context.setSearchByTitle(e.target.value)}
+      />
       <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
         {
-          items?.map((item) => (
+          context.filteredItems?.map((item) => (
             <Card
               id={item.id}
               key={item.id}
