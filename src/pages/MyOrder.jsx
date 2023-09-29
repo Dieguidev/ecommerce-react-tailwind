ac4e95b283e26cef133f14673e1ee7b80ea6cbb3
Dieguidev <diegogaraycullas@gmail.com>
2023-09-29 00:06:26 -0500

    se agrego funcion detalle de compre

--- src/pages/MyOrder.jsx
+++ src/pages/MyOrder.jsx
@@ -1,26 +1,42 @@
 import { useContext } from "react"
 import { ShoppingCartContext } from "../context"
 import { OrderCard } from "../components/OrderCard"
+import { Link } from "react-router-dom"
+import { ChevronLeftIcon } from "@heroicons/react/24/solid"
 
 export const MyOrder = () => {
 
   const context = useContext(ShoppingCartContext)
-  console.log(context.order.slice(-1)[0]?.products);
+
+  const currentPath = window.location.pathname
+  let index = currentPath.substring(currentPath.lastIndexOf('/') + 1)
+
+  if (index === 'last') index = context.order?.length - 1
 
   return (
-    <div className='flex flex-col w-80'>
-      {
-        context.order.slice(-1)[0]?.products.map((product) => (
-          <OrderCard
-            title={product.title}
-            price={product.price}
-            imageUrl={product.image1}
-            key={product.id}
-            id={product.id}
-          />
-        )
-        )
-      }
+    <div>
+      <div className='flex items-center w-80 mb-2 relative justify-center'>
+        <Link to='/my-orders' className='absolute left-0'>
+          <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer"  ></ChevronLeftIcon>
+        </Link>
+        <h1>My Order</h1>
+
+      </div>
+      <div className='flex flex-col w-80'>
+        {
+          context.order[index]?.products.map((product) => (
+            <OrderCard
+              title={product.title}
+              price={product.price}
+              imageUrl={product.image1}
+              key={product.id}
+              id={product.id}
+            />
+          )
+          )
+        }
+      </div>
     </div>
+
   )
 }
--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,6 +1,28 @@
+import { useContext } from "react"
+import { ShoppingCartContext } from "../context"
+import { OrderCard } from "../components/OrderCard"
+import { Link } from "react-router-dom"
 
 export const MyOrders = () => {
+  const context = useContext(ShoppingCartContext)
   return (
-    <div>MyOrders</div>
+    <div>
+      <div className='flex items-center w-80 relative justify-center'>
+        <h1>My Orders</h1>
+      </div>
+      <div>
+        {
+          context.order.map((order, index) => (
+            <Link key={index} to={`/my-orders/${index}`}>
+              <OrderCard
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalProducts}
+              />
+            </Link>
+          ))
+        }
+      </div>
+
+    </div>
   )
 }
